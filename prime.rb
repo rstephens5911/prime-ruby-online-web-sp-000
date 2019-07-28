58840566325dac7e1d63dd1954ea5f67436e78e2
Ronald Stephens <rstephens@bluemorpho.cc>
2019-07-28 20:52:25 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,10 @@
 def prime?(int)
   if int <= 1
     return false
-  else
-      if (2...int).any? do |i|
+  elsif (2...int).any? do |i|
       int % i == 0
       return false
-    else
+  else
       return true
     end
   end
