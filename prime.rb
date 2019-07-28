2e636305caf6915536a57d585efa4cccc4673499
Ronald Stephens <rstephens@bluemorpho.cc>
2019-07-28 20:38:48 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,9 +3,9 @@ def prime?(int)
 
   range.each do |num|
     if int / num == 1 || int / num == -1 && int % num != 0
-      return true
-    else
       return false
+    else
+      return true
     end
   end
 end
