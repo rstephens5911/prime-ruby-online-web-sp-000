61a198c990b95994016db72d88aee4cbde11a8e5
Ronald Stephens <rstephens@bluemorpho.cc>
2019-07-28 20:58:43 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(int)
+  if int <= 1
+    return false
+  else
+    if (2...int).any? do |i|
+      int % i == 0
+    end
+      return false
+  else
+      return true
+    end
+  end
+end
