4a1df2830da7764bf1c74c38a43f1dea771a24f4
Ronald Stephens <rstephens@bluemorpho.cc>
2019-07-28 20:49:36 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,14 +1,13 @@
 def prime?(int)
-  range = [1..100]
-
-  range.each do |num|
-    numb = num.to_i
-    if int <= 1
-      return false
-    elsif int % numb == 0
+  if int <= 1
+    return false
+  else
+    if (2...int).any? do |i|
+      int % i == 0
       return false
     else
       return true
     end
   end
 end
+
